33bf585225be4a7b78656fa0becc9b854a972323
Brian Michel <brian.michel@gmail.com>
2019-06-22 09:17:47 -0400

    Initial Commit

---
+++ Tote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tote
+//
+//  Created by Brian Michel on 6/22/19.
+//  Copyright © 2019 Brian Michel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToteTests/ToteTests.swift
@@ -0,0 +1,34 @@
+//
+//  ToteTests.swift
+//  ToteTests
+//
+//  Created by Brian Michel on 6/22/19.
+//  Copyright © 2019 Brian Michel. All rights reserved.
+//
+
+import XCTest
+@testable import Tote
+
+class ToteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
