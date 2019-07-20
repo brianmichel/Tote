08846fee54388012f578f5c52fb3c4cdecd84a63
Brian Michel <brian.michel@gmail.com>
2019-07-20 18:21:19 -0400

    Adding entitlements

--- Tote/Models/Responses/PhotosListResponse.swift
+++ Tote/Models/Responses/PhotosListResponse.swift
@@ -28,28 +28,6 @@ final class Folder: Codable {
         files.reversed()
     }()
 
-    lazy var photoGroups: [PhotoGroup] = {
-        let countedSet = NSCountedSet(capacity: files.count)
-        for file in files {
-            let withoutSuffix = file.dropLast(4)
-            countedSet.add(withoutSuffix)
-        }
-
-        let groups = countedSet.sortedArray(using: [NSSortDescriptor(key: "hash", ascending: false)]).map { group -> PhotoGroup in
-            let count = countedSet.count(for: group)
-            let endGroup = group as? String ?? "okay"
-            var type = PhotoGroupType.rawPlusJpg
-
-            if count == 1 {
-                type = .raw
-            }
-
-            return PhotoGroup(baseName: endGroup, folder: name, type: type)
-        }
-
-        return groups
-    }()
-
     enum CodingKeys: String, CodingKey {
         case name
         case files
